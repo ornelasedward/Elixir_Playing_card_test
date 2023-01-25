1f2ccdd9fcbcb6b5e5f254941c25784cb56637da
Edward Ornelas <ornelasedward@rocketmail.com>
2023-01-24 18:20:44 -0600

    iterating throught two array

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,8 +2,24 @@
 defmodule Cards do
 
     def create_deck do
-      ["Ace", "two", "three", "four", "five", "six"]
+      values = ["Ace", "two", "three", "four", "five", "six"]
+      suits = ["Spades", "Clubs", "Hearts", "Diamomds"]
+
+      #cards = for value <- values do
+      #       for suit <- suits do
+      #
+      #       "#{value} of #{suit}"
+      # end
+      # end
+
+      # List.flatten(cards)
+
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+      end
     end
+
+
 
     #shuffle method
     def shuffle_cards(deck) do
